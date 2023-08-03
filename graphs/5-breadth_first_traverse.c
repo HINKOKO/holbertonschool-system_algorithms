78d10c1027c802375e8d71ae8fd6816a78146c3b
HINKOKO <piotr62@live.fr>
2023-08-03 10:15:50 +0200

    TEsting BFS

--- graphs/5-breadth_first_traverse.c
+++ graphs/5-breadth_first_traverse.c
@@ -1,88 +1,6 @@
 #include "graphs.h"
 
 /**
- * push - push a vertex in the queue
- * @head: double pointer to head node of queue we want to push in
- * (modify such a struct -> always double pointer on head)
- * @tail: pointer to end of queue
- * @v: pointer to vertex to be added
- * Return: pointer to new elements in queue
- */
-
-queue_t *push(queue_t **head, queue_t *tail, vertex_t *vrtx)
-{
-	queue_t *new_v = malloc(sizeof(queue_t));
-
-	if (!new_v)
-		return (NULL);
-	new_v->next = NULL;
-	new_v->v = vrtx;
-	if (*head)
-		tail->next = new_v;
-	else
-		*head = new_v;
-}
-
-/**
- * pop - performs a pop on queue // FIFO
- * @head: double pointer to head of queue we're modifying
- * Return: pointer to popped vertex
- */
-
-vertex_t *pop(queue_t **head)
-{
-	queue_t *tmp;
-	vertex_t *vrtx;
-
-	if (*head)
-	{
-		tmp = *head;
-		/* Do not loose your head */
-		*head = (*head)->next;
-		vrtx = tmp->v;
-		free(tmp);
-	}
-	return (vrtx);
-}
-
-/**
- * BFS_recurs - core stuff of BFS mecanics
- * @visit: pointer to vertex being visited now
- * @visited: pointer to tracking array of visited vertex
- * @action: pointer to function call for each visited vertex
- * Return: the max breadth possibly reachable of the graph
- */
-
-size_t BFS_recurs(vertex_t *visit, int *visited,
-				  void (*action)(const vertex_t *v, size_t depth))
-{
-	/* pointer for visiting edges */
-	edge_t *ed = NULL;
-	/* for return value */
-	size_t max_breadth = 0;
-	/* init a queue */
-	queue_t *head = NULL, *tail = NULL;
-	if (!visit)
-		return (0);
-	/* mark current as visited, max is 1 when restart with another one */
-	visited[visit->index] = max_breadth = 1;
-	/* perform the call for each vertex to action */
-	action(visit, 0);
-	ed = visit->edges;
-	while (ed)
-	{
-		if (visited[ed->dest->index] != 1)
-			tail = push(&head, tail, ed->dest);
-		ed = ed->next;
-	}
-	for (ed = head->v->edges; ed; ed = ed->next)
-		;
-	head = action(pop(&head), ???);
-
-	GLARRRBARRRRWAKIYAAAAAAA
-}
-
-/**
  * breadth_first_traverse - goest through a graph using breadth-first
  * @graph: pointer to graph to breadth-first'ss
  * @action: pointer to a function to be called for each visited vertex
@@ -92,15 +10,42 @@ size_t BFS_recurs(vertex_t *visit, int *visited,
 size_t breadth_first_traverse(const graph_t *graph,
 							  void (*action)(const vertex_t *v, size_t depth))
 {
-	int *visited;
-	size_t max_breadth = 0;
+	size_t depth = 0, next_b = 0, b_size = 1;
+	char *mark = NULL;
+	vertex_t **vrtx;
+	edge_t *ed;
+	int i, j = 1;
 
-	if (!graph)
+	if (!graph || !graph->nb_vertices)
 		return (0);
-	visited = malloc(sizeof(*visited) * graph->nb_vertices);
-	if (!visited)
-		return (0);
-	max_breadth = BFS_recurs(graph->vertices, visited, action);
-	free(visited);
-	return (max_breadth);
+
+	mark = calloc(graph->nb_vertices, sizeof(char));
+	vrtx = calloc(graph->nb_vertices, sizeof(vertex_t *));
+
+	vrtx[0] = graph->vertices;
+	mark[0] = 1;
+
+	/* outer loop to go through all vertex in main LL */
+	for (i = 0; vrtx[i]; i++)
+	{
+		action(vrtx[i], depth);
+		/* inner loop to go through LL of edges paths */
+		for (ed = vrtx[i]->edges; ed; ed = ed->next)
+		{
+			/* vertex found and not seen already */
+			if (mark[ed->dest->index] == 0)
+			{
+				vrtx[j++] = ed->dest;
+				mark[ed->dest->index] = 1;
+				next_b += 1;
+			}
+		}
+		if (--b_size == 0)
+		{
+			depth++, b_size = next_b;
+			next_b = 0;
+		}
+	}
+	free(mark), free(vrtx);
+	return (depth);
 }
