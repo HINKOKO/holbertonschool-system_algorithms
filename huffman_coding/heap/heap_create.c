c709ff1e5b981e6b854ec9ced4259127b2894bcd
HINKOKO <piotr62@live.fr>
2023-08-15 11:01:36 +0200

    update create with correct size parameter

--- huffman_coding/heap/heap_create.c
+++ huffman_coding/heap/heap_create.c
@@ -17,12 +17,13 @@ heap_t *heap_create(int (*data_cmp)(void *, void *))
 
 	(void)data_cmp;
 
-	new_heap = malloc(sizeof(heap_t));
+	new_heap = malloc(sizeof(*new_heap));
 	if (!new_heap)
 		return (NULL);
 
 	new_heap->root = NULL;
 	new_heap->size = 0;
+	new_heap->data_cmp = data_cmp;
 
 	return (new_heap);
 }
