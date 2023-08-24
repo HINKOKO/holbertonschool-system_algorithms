6c6bb3a22d30c66f5c4a2aae05f3b5a1cba0dbc6
HINKOKO <piotr62@live.fr>
2023-08-24 11:04:42 +0200

    restart huffman_driver, simply base case let's go

--- huffman_coding/huffman/huffman_driver.c
+++ huffman_coding/huffman/huffman_driver.c
@@ -6,34 +6,28 @@
 #define COMPRESS 1
 #define DECOMPRESS 2
 
-int check_mode(int argc, char **argv)
-{
-}
-
-int check_access(int argc, char **argv)
-{
-	(void)argc;
-
-	if (access(argv[2], F_OK) == -1)
-		return (fprintf(stderr, "No such file: %s", argv[2]), EXIT_FAILURE);
-}
+/* int access(const char *pathname, int mode); */
+/* F_OK mode check for existence of the file */
 
 int main(int argc, char **argv)
 {
-	int ret = 0, mode = 0;
+	int ret = EXIT_FAILURE;
 
-	if (argc != 4 || *argv[1] != 'c' || *argv[1] != 'd')
-		return (fprintf(stderr, "Usage: huffman <mode> <filename> <out_filename>\n", EXIT_FAILURE));
-	ret = check_access(argc, argv);
-	if (ret != 0)
-		return (EXIT_FAILURE);
-	mode = check_mode(argc, argv);
-	if (mode == 0)
-		return (EXIT_FAILURE);
-	if (mode == 1)
-		compress(argv[2], argv[3]);
-	else if (mode == 2)
-		decompress(argv[2], argv[3]);
+	if (argc != 4)
+		return (fprintf(stderr, "Usage: huffman <mode> <filename> <out_filename>\n"), EXIT_FAILURE);
 
-	return (EXIT_SUCCESS);
+	if (access(argv[2], F_OK) == -1)
+		return (fprintf(stderr, "No such file: %s\n", argv[2]), EXIT_FAILURE);
+
+	if (access(argv[3], F_OK) != -1)
+		return (fprintf(stderr, "File already exists: %s\n", argv[3]), EXIT_FAILURE);
+
+	if (!strcmp(argv[1], "c"))
+		printf("Compression mode detected, please wait...\n");
+	else if (!strcmp(argv[1], "d"))
+		printf("Decompression mode detected, please wait...\n");
+	else
+		printf("Mode usage: either \"c\" for compression, or \"d\" for decompression.Take care\n");
+
+	return (ret);
 }
