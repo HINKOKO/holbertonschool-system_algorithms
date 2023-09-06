979058b95fb26278c5eef48d1dda302d45474557
HINKOKO <piotr62@live.fr>
2023-09-06 10:01:53 +0200

    pushing files to retrieve in sandbox for testing...

---
+++ pathfinding/1-backtracking_graph.c
@@ -0,0 +1,20 @@
+#include "pathfinding.h"
+
+int backtrack_helper(queue_t *path, char *visited, vertex_t const *current,
+					 vertex_t const *target)
+{
+}
+/**
+ * backtracking_graph - search for the first path from start to target in a graph
+ *
+ * @graph: pointer to the graph
+ * @start: pointer to starting vertex
+ * @targer: pointer to desired vertex to reach
+ *
+ * Return: must return a queue, with each node (char *) corresponding
+ * to a vertex, forming a path from 'start' to 'target'
+ *
+ */
+queue_t *backtracking_graph(graph_t *graph, vertex_t const *start, vertex_t const *target)
+{
+}
