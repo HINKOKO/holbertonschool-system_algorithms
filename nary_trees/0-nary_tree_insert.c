31548eb9925abdc3c3ee4d13b18eaf3044307f43
HINKOKO <piotr62@live.fr>
2023-10-17 15:46:00 +0200

    Bettylifting + provides main program

--- nary_trees/0-nary_tree_insert.c
+++ nary_trees/0-nary_tree_insert.c
@@ -4,7 +4,7 @@
  * nary_tree_insert - inserts a node in a N-ary tree
  * @parent: pointer to the parent node (of inserted one)
  * @str: string to be stored in that node (duplicated)
- * Return: pointer to created node / NULL on failure 
+ * Return: pointer to created node / NULL on failure
 */
 
 nary_tree_t *nary_tree_insert(nary_tree_t *parent, char const *str)
@@ -13,7 +13,7 @@ nary_tree_t *nary_tree_insert(nary_tree_t *parent, char const *str)
 
 	if (!new)
 		return (fprintf(stderr, "Failed to alloc memory for a new node\n"), NULL);
-	
+
 	new->parent = parent;
 	new->content = strdup(str);
 	new->nb_children = 0;
